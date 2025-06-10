130dff873f97b4b5e2e969d9de05f27a9bf0cdbc
Nicollas Henrique <164679510+Nicoo200@users.noreply.github.com>
2025-06-10 20:54:43 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'clinicapi-h4bfgdfac8bcfpbt.brazilsouth-01.azurewebsites.net',
 });
 
 export default api;
