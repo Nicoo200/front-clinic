8a5b5a1a683ddf7987a024c9ef18b8fdf6a1b6c5
Nicollas Henrique <164679510+Nicoo200@users.noreply.github.com>
2025-06-16 14:37:42 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'clinicapi-h4bfgdfac8bcfpbt.brazilsouth-01.azurewebsites.net',
+  baseURL: 'clinicapi-h4bfgdfac8bcfpbt.brazilsouth-01.azurewebsites.net/auth/login',
 });
 
 export default api;
