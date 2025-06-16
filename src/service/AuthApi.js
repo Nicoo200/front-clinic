427783bf1aaf4e2e7c5f720d22c03b44e6e62c5e
Nicollas Henrique <164679510+Nicoo200@users.noreply.github.com>
2025-06-16 14:30:35 -0300

    Update AuthApi.js

--- src/service/AuthApi.js
+++ src/service/AuthApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'clinicapi-h4bfgdfac8bcfpbt.brazilsouth-01.azurewebsites.net/auth'; 
+const BASE_URL = 'https://clinicapi-h4bfgdfac8bcfpbt.brazilsouth-01.azurewebsites.net/auth'; 
 
 export async function cadastrarUsuario(usuario) {
   const response = await axios.post(`${BASE_URL}/users`, usuario);
